ba747b2fc8bfd72c7c63cc64dc740c6ed0480b7e
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-01 15:59:40 -0500

    Ejercicio Multiplicación y creación de archivos con FileSystem

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log('Hello World');
+import fs from 'fs';
+
+let outputMessage = '';
+const num:number = 5;
+const headerMessage = `
+====================================
+Tabla del ${num}
+====================================
+`;
+
+for(let i = 1; i<=10; i++){
+    outputMessage+=`${num} x ${i} = ${num*i}\n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+console.log(outputMessage);
+
+
+const outputPath = 'outputs';
+fs.mkdirSync(outputPath,{recursive:true});//Crea las carpetas requeridas por outputPath
+fs.writeFileSync(`${outputPath}/tabla-${num}.txt`,outputMessage);
+console.log('Archivo creado!');
