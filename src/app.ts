c58e296464d811b81ae4fe6ecf0a969d325f2fb5
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-01 16:18:30 -0500

    Argv-Argumentos TS

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,5 @@
-import fs from 'fs';
-
-let outputMessage = '';
-const num:number = 5;
-const headerMessage = `
-====================================
-Tabla del ${num}
-====================================
-`;
-
-for(let i = 1; i<=10; i++){
-    outputMessage+=`${num} x ${i} = ${num*i}\n`;
-}
-
-outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
-
-
-const outputPath = 'outputs';
-fs.mkdirSync(outputPath,{recursive:true});//Crea las carpetas requeridas por outputPath
-fs.writeFileSync(`${outputPath}/tabla-${num}.txt`,outputMessage);
-console.log('Archivo creado!');
+console.log(process.argv);
+const [tsnode, app, ...args] = process.argv;
+console.log(tsnode);
+console.log(app);
+console.log(args);
