fda53dcda2cd4fc6dce6745a4b95e8279829c724
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-02 17:36:33 -0500

    Yargas configurado con opciones

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
 import { yarg } from "./config/plugins/args.plugin";
 
-console.log(process.argv);
-console.log(yarg);
+//console.log(process.argv);
+//console.log(yarg);
+
+//Funcion asincrona que espera a que se ejecute la funcion main
+(async()=>{
+    await main();
+    console.log('Finalizado');
+})();
+
+async function main(){
+    console.log('Main Ejecutado');
+    console.log(yarg);
+}
--- src/config/plugins/args.plugin.ts
+++ src/config/plugins/args.plugin.ts
@@ -1,4 +1,23 @@
 import yargs from 'yargs';
 import {hideBin} from 'yargs/helpers';
 
-export const yarg = yargs(process.argv).parseSync();
+export const yarg = yargs(process.argv)
+.option('m',{
+    alias:'num',
+    type:'number',
+    demandOption:true,
+    describe:'Multiplication table base number'
+})
+.option('l',{
+    alias:'limit',
+    type:'boolean',
+    default:5,
+    describe:'List the multiplication table'
+})
+.option('s',{
+    alias:'show',
+    type:'boolean',
+    default:false,
+    describe:'Show multiplication table on console'
+})
+.parseSync();
