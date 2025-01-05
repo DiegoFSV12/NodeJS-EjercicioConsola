629037cddf00214b069c55045ee34625ba5eb9c3
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-05 15:40:14 -0500

    SaveFile Testing

--- src/domain/use-cases/save-file.test.ts
+++ src/domain/use-cases/save-file.test.ts
@@ -39,4 +39,22 @@ describe('SaveFile',()=>{
         expect(check).toBe(true);
         expect(fileContent).toBe(options.fileContent);
     });
+    test('should return false if directory could not be created',()=>{
+        const saveFile = new SaveFile();
+        const mkdirSpy = jest.spyOn(fs,'mkdirSync').mockImplementation(()=>{
+            throw new Error('This is a custom error message from testing');
+        });
+        const result = saveFile.execute({fileContent:'TEST'});
+        expect(result).toBe(false);
+        mkdirSpy.mockRestore();
+    });
+    test('should return false if directory could not be created',()=>{
+        const saveFile = new SaveFile();
+        const writeFileSpy = jest.spyOn(fs,'writeFileSync').mockImplementation(()=>{
+            throw new Error('This is a custom error message from testing');
+        });
+        const result = saveFile.execute({fileContent:'TEST'});
+        expect(result).toBe(false);
+        writeFileSpy.mockRestore();
+    });
 });
