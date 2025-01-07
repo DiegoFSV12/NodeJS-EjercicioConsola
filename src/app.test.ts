23b9c4dd2ec48bb6e1fb37caa5c034e873a7de8f
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-07 14:07:40 -0500

    Testing App

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,21 @@
+import {ServerApp} from './presentation/server-app';
+
+
 describe('App',()=>{
-    test('should be true',()=>{
-        expect(true).toBe(true);
+    test('should call Server.run with values',async()=>{
+        const serverRunMock = jest.fn();
+        const logMock = jest.fn();
+        ServerApp.run = serverRunMock;
+        console.log = logMock;
+        process.argv = ['node','app.ts','-m','10','-l','15','-s','-n','testFile','-o','testDestiny'];
+        await import('./app');
+        expect(serverRunMock).toHaveBeenCalledWith({
+            num:10,
+            limit:15,
+            show:true,
+            name:'testFile',
+            destination:'testDestiny'
+        });
+        expect(logMock).toHaveBeenCalledTimes(1);
     })
 })
